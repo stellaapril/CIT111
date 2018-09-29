ff9cb0072655d85d32cdd8c0033ce877556ed48d
stellaapril <43618328+stellaapril@users.noreply.github.com>
2018-09-28 20:12:43 -0400

    MightWeBeFriends?
    CIT111

--- FriendsWithUserInput.java
+++ FriendsWithUserInput.java
@@ -3,67 +3,124 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controlstructure;
+package MightWeBeFriends;
 
+//import scanner
 import java.util.Scanner;
+
 /**
  *
- * @author sitian.chen
+ * @author cst
  */
 public class FriendsWithUserInput {
     public static void main(String[] args){
+        System.out.println("********************");
         System.out.println("Might we be friends?");
-        //container for storing user input.No initial value given.
-        int RESPONSE_HIKING;
-        int RESPONSE_PHOTOGRAPHY;
-        int compScore = 0;
+        System.out.println("********************");
+    //container for Q1
+    int Q1petquestion;
+    // compatability score
+    int CompScore = 0;
+    
+    //crate a scanner object and store
+    Scanner userInput = new Scanner(System.in);
+    System.out.println("________________________________________________________");
+    System.out.println("Q1:Do you like pets? Yes=1 No=2");
+    System.out.println("________________________________________________________");
+    
+    Q1petquestion = userInput.nextInt( );
+    //start decision logic
+    if(Q1petquestion==1){
+        System.out.println("Hey, me too!\n");
+        CompScore = CompScore + 10;
         
-        //Scanner
-        Scanner keyboardReader = new Scanner(System.in);
-
+        System.out.println("________________________________________________________");
+        System.out.println("Q1-2:Do you prefer dog or cat? Dog=1 Cat=2");
+        System.out.println("________________________________________________________");
+        //declare Q2 and create a scanner object
+        int Q1dogquestion;
+        Q1dogquestion = userInput.nextInt( );
         
-        System.out.println("Q1/1:Do you like hiking? (0=no,1=yes)");
+        if(Q1dogquestion == 1){
+            System.out.println("Great! I'm a dog people,too.\n");
+            CompScore = CompScore + 5;
+        }else{System.out.println("You might be a cat person!\n");
+            CompScore = CompScore - 5;
+        }//close Q1-2 if/else
         
-        RESPONSE_HIKING= keyboardReader.nextInt();
+    }else{ System.out.println(" :( ");
+    }//close Q1 if/else
+    System.out.println("________________________________________________________");
+    System.out.println("Q2:Do you like to watch movies? Yes=1 No=2");
+    System.out.println("________________________________________________________");
+    
+    int Q2moviequestion;
+    Q2moviequestion = userInput.nextInt( );
+    
+    if(Q2moviequestion == 1){
+        System.out.println("Wow!\n");
+        CompScore = CompScore + 10;
+        System.out.println("_____________________________________________________________________________________________");
+        System.out.println("Q2-2: Which type of movie do you prefer? Action, Comedy or Romance?(Choose one and type it)");
+        System.out.println("_____________________________________________________________________________________________");
+        String Q2typequestion;
+        String Action;
+        String Comedy;
+        String Romance;
+        Q2typequestion = userInput.next( );
         
-        if(RESPONSE_HIKING==1){
-            compScore = compScore + 10;
-            System.out.println("Great!");
-            System.out.println("Your compatability score is:"+ compScore);
-        }else{
-            System.out.println("...you must like it if you tired!");
-        }//end if/else blocks
+        //response Action
+        if(Q2typequestion.equals ("Action")){
+            System.out.println("Cool!\n");
+            CompScore = CompScore + 5;
+        }//close if
+        //response Comedy
+        if(Q2typequestion.equals("Comedy")){
+            System.out.println(":(\n");
+            CompScore = CompScore - 5;
+        }//close if
+        //response Romance
+        if(Q2typequestion.equals("Romance")){
+            System.out.println("Great! It is my favorite.\n");
+            CompScore = CompScore + 10;
+        } //close if
         
-        System.out.println("Q2/2:Do you like photograpy?(0=no,1=yes)");
-            RESPONSE_PHOTOGRAPHY = keyboardReader.nextInt();
-            
-            if(RESPONSE_PHOTOGRAPHY == 1){
-            compScore = compScore + 10;
-            System.out.println("Great!");
-            System.out.println("Your compatability score is:"+ compScore);
-        } else {
-            System.out.println("...you must like it if you tired!");
-        }//end if/else blocks
-            
-            
-            
-            
-            
-        if(compScore > 10) {
-            System.out.println("We Might Be Friends!");
-        } else{ 
-            System.out.println("We Probably Won't Be Friends.");
-        }
-
+    }else{System.out.println(" :( ");
+    }//close Q2 if/else
+    
+    System.out.println("________________________________________________________");
+    System.out.println("Q3: Do you like travel? Yes=1 No=2");
+    System.out.println("________________________________________________________");
+    
+    //declare Q3
+    int Q3travelquestion;
+    Q3travelquestion = userInput.nextInt( );
+    
+    if(Q3travelquestion == 1){
+        System.out.println("Great!\n");
+        CompScore = CompScore + 10;
         
-        
-        
-        
-        
-            
-                    
-                    
-        }
-        
-        
-}
+    }else{System.out.println(":(\n");
+    }//closeQ3 if/else
+    
+    //check of score
+    if(CompScore<20){
+        System.out.println("**********************************************************************");
+        System.out.println(":( According to our compatability score, we probably won't be friends.");
+        System.out.println("**********************************************************************");
+    }//close if
+    
+    if(CompScore>=20 && CompScore<40){
+        System.out.println("******************************************************");
+        System.out.println("Based on our compatability score, we might be friends!");
+        System.out.println("******************************************************");
+    }// close if  
+    
+    if(CompScore>=40){
+        System.out.println("********************************************************************");
+        System.out.println("You get a high score in my decision tree! We might be good friends!:)");
+        System.out.println("********************************************************************");
+    }//close if
+    }//close method
+    
+}//close class
