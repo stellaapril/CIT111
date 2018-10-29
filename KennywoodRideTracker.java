5c20ee94d4726e0a3a50b7a0dd5fbc48f96dda3c
stellaapril <43618328+stellaapril@users.noreply.github.com>
2018-10-29 13:56:33 -0400

    kennywood2

--- KennywoodRideTracker.java
+++ KennywoodRideTracker.java
@@ -5,36 +5,58 @@
  */
 package ls3_classStructure;
 
-import java.util.Random
+import java.util.Random;
+        /**
+         *
+         * @author sitian.chen
+         */
 
-/**
- *
- * @author sitian.chen
- */
 public class KennywoodRideTracker {
+
     private static int totalRiders = 0;
     private static int totalFailedRideAttempts = 0;
     private static int totalSickRiders = 0;
     public static final int THRILL_MINHEIGHT = 7;
-    public static final int THEME_MINHEIGHT =6;
+    public static final int THEME_MINHEIGHT = 6;
     
-    public static void rideBlackWidow(int riders, int avgHeight){
-        if(avgHeight>=THRILL_MINHEIGHT){
+    public static void main(String[] args){
+        printRideStats();
+    }
+
+    public static void rideBlackWidow(int riders, int avgHeight) {
+        if (avgHeight >= THRILL_MINHEIGHT) {
             System.out.println("Riding black widow!");
-            totalRiders = totalRiders+1;
-            
-            
-        }
-        
-        
-    }//close method2
-    
+            totalRiders = totalRiders + riders;
+            Random rand = new Random();
+            int numSickRiders = rand.nextInt(riders);
+            totalSickRiders = totalSickRiders + numSickRiders;
+        } else {
+            System.out.println(riders + "risers turned away asd : too short!\n");
+            totalFailedRideAttempts = totalFailedRideAttempts + riders;
+
+        }//close if/else
+    }//close method
+
+
+
     public static void rideMerryGoRound(int riders, int avgHeight){
-        
+        if(avgHeight>=THEME_MINHEIGHT){
+            System.out.println("Riding MerryGoRound!");
+            totalRiders = totalRiders + riders;
+            Random rand = new Random();
+            int numSickRiders = rand.nextInt(riders);
+            totalSickRiders = totalSickRiders + numSickRiders;
+        }else{
+            System.out.println(riders + "risers turned away asd : too short!\n");
+            totalFailedRideAttempts = totalFailedRideAttempts + riders;
+        }//close if/else
     }//close method3
     
     public static void printRideStats(){
+        System.out.println("Total riders: "+totalRiders);
+        System.out.println("Total Failed Ride Attempts: "+totalFailedRideAttempts);
+        System.out.println("Total sick riders: "+totalSickRiders);
         
     }//close method4
     
-}
+}//class close
