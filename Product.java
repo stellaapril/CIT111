95edbce767132d2120552938c529dd60f33cfdc9
stellaapril <43618328+stellaapril@users.noreply.github.com>
2018-12-10 04:42:10 -0500

    Final1210

--- Product.java
+++ Product.java
@@ -3,17 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package finalproject;
+package CIT111Final;
 
 /**
  *
- * @author sitian.chen
+ * @author cst
  */
 public class Product {
     //public variables
     public double price;
     public String number;
     public String name;
+    public String size;
     
     //private variables
     private boolean isProductinstock;
@@ -40,6 +41,38 @@ public class Product {
             isProductinstock = false;
         }//close if/else
         return isProductinstock;
+    }//close method
+    
+    public int getRemainingproduct(){
+        return productRemain;
+    }//close method
+    
+    public static void displaymachie(){
+        System.out.println("   _____________________________");
+        System.out.println("  /                            /|");
+        System.out.println(" /                            / |");
+        System.out.println("/____________________________/  |");
+        System.out.println("|  A01  |   A02  |  A03 |   |   |");
+        System.out.println("|_______________________|   |   |");
+        System.out.println("|  B01  |   B02  |  B03 |   |   |");
+        System.out.println("|_______________________|---|   |");
+        System.out.println("|  C01  |   C02  |  C03 | | |   |");
+        System.out.println("|_______________________| | |   |");
+        System.out.println("|  D01  |   D02  |  D03 |---|   |");
+        System.out.println("|_______________________|   |   |");
+        System.out.println("|                       |   |  /");
+        System.out.println("|_______________________|   | / ");
+        System.out.println("|_______________________|___|/ ");
+        System.out.println("                                ");
+        
+    }//close method
+    
+    public static void displayprice(String productNum,String name,double price){
+        System.out.println("******************************************");
+        System.out.println(productNum+" : "+name);
+        System.out.println("Total Price: "+price+" $");
+        System.out.println("You can use Credit Card or 5$/10$/20$ cash");
+        System.out.println("******************************************");
     }//close method
     
 }//close class
--- VendingMachine.java
+++ VendingMachine.java
@@ -3,38 +3,137 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package finalproject;
+package CIT111Final;
 
 import java.util.HashMap;
 
+import java.util.Scanner;
+
 /**
  *
- * @author sitian.chen
+ * @author cst
  */
 public class VendingMachine {
     public static void main(String[] args){
-        Product coke = new Product();
-        coke.name = "coca cola";
-        coke.price = 2.75;
-        coke.number = "A01";
+        Product doricheese = new Product();
+        doricheese.name = "Doritos Cheese";
+        doricheese.price = 1.75;
+        doricheese.size = "1.25oz";
+        doricheese.number = "A01";
         
-        Product chip = new Product();
-        chip.name = "Lays Classic Chip";
-        chip.price = 1.99;
-        chip.number = "B01";
+        Product layclassic = new Product();
+        layclassic.name = "Lay's Chip Classic";
+        layclassic.price = 1.75;
+        layclassic.size = "1.5oz";
+        layclassic.number = "A02";
+        
+        Product smartpop = new Product();
+        smartpop.name = "Smartfood Popcorn";
+        smartpop.price = 1.5;
+        smartpop.size = "2oz";
+        smartpop.number = "A03";
+        
+        Product mmchocolate = new Product();
+        mmchocolate.name = "M&M milk chocolate";
+        mmchocolate.price = 1.25;
+        mmchocolate.size = "1.69oz";
+        mmchocolate.number = "B01";
+        
+        Product oreo = new Product();
+        oreo.name = "Oreo Cookies Vanilla Cream";
+        oreo.price = 2;
+        oreo.size = "70g";
+        oreo.number = "B02";
+        
+        Product nuts = new Product();
+        nuts.name = "Planters Salted nuts";
+        nuts.price = 1.5;
+        nuts.size = "1.5oz";
+        nuts.number = "B03";
+        
+        Product coke = new Product();
+        coke.name = "Coca Cola";
+        coke.price = 1.75;
+        coke.size = "20 fl oz";
+        coke.number = "C01";
+        
+        Product dietcoke = new Product();
+        dietcoke.name = "Dite Coke";
+        dietcoke.price = 1.75;
+        dietcoke.size = "20 fl oz";
+        dietcoke.number = "C02";
+        
+        Product sprite = new Product();
+        sprite.name = "Sprite";
+        sprite.price = 1.75;
+        sprite.size = "20 fl oz";
+        sprite.number = "C03";
+        
+        Product vitaminzero = new Product();
+        vitaminzero.name = "Vitamin Water Zero pineapple coconut";
+        vitaminzero.price = 2.25;
+        vitaminzero.size = "20 fl oz";
+        vitaminzero.number = "D01";
+        
+        Product drpep = new Product();
+        drpep.name = "Dr.Peper";
+        drpep.price = 1.75;
+        drpep.size = "20 fl oz";
+        drpep.number = "D02";
+        
+        Product water = new Product();
+        water.name = "Dasani Purified Water";
+        water.price = 1.75;
+        water.size = "12 fl oz";
+        water.number = "D03";
+        
+    
+        
         
         //hashmap
         HashMap<String,Product> productmap = new HashMap<>();
+        productmap.put(doricheese.number,doricheese);
+        productmap.put(layclassic.number,layclassic);
+        productmap.put(smartpop.number,smartpop);
+        productmap.put(mmchocolate.number,mmchocolate);
+        productmap.put(oreo.number,oreo);
         productmap.put(coke.number,coke);
-        productmap.put(chip.number,chip);
+        productmap.put(dietcoke.number,dietcoke);
+        productmap.put(sprite.number,sprite);
+        productmap.put(vitaminzero.number,vitaminzero);
+        productmap.put(drpep.number,drpep);
+        productmap.put(water.number,water);
         
-        Product retrivedproduct = productmap.get("B01");
-        System.out.println(retrivedproduct.name);
-        System.out.println(retrivedproduct.price);
+        /**
+        Product retrivedproductA01 = productmap.get("A01");
+        System.out.println(retrivedproductA01.name);
+        System.out.println(retrivedproductA01.price);
         
-        Product retrivedproduct2 = productmap.get("A01");
-        System.out.println(retrivedproduct2.name);
-        System.out.println(retrivedproduct2.price);
+        Product retrivedproductA02 = productmap.get("A02");
+        System.out.println(retrivedproductA02.name);
+        System.out.println(retrivedproductA02.price);
+        
+        Product retrivedproductA03 = productmap.get("A03");
+        System.out.println(retrivedproductA03.name);
+        System.out.println(retrivedproductA03.price);
+        
+        */
+        Product.displaymachie();
+        System.out.println("Enter the number of item ");
+        Scanner userInput = new Scanner(System.in);
+        String productNum = userInput.next( );
+        Product retrivedproductX = productmap.get(productNum);
+        Product.displayprice(productNum,retrivedproductX.name,retrivedproductX.price);
+        
+        
     }//close main
+    
+    
+    
+    
+    
+    public static void displaychange(){
+        
+    }//close method
     
 }//close class
